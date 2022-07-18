8b57219e0ee7cee996d9e09dfdf23a0503876898
hide0621 <hi19de72h2so4@gmail.com>
2022-07-18 16:52:06 +0900

    typoの修正

--- go.mod
+++ go.mod
@@ -5,6 +5,13 @@ go 1.18
 require github.com/labstack/echo v3.3.10+incompatible
 
 require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.12 // indirect
+)
+
+require (
+	github.com/jinzhu/gorm v1.9.16
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -14,4 +21,6 @@ require (
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
 	golang.org/x/text v0.3.6 // indirect
+	gorm.io/driver/sqlite v1.3.6
+	gorm.io/gorm v1.23.8
 )
--- main.go
+++ main.go
@@ -4,24 +4,55 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/labstack/echo"
 )
 
-//以下の書き方は構文のようなもの
-func main() {
+type Content struct {
+	gorm.Model
+	Text string `json:"text"`
+}
 
-	// echoパッケージにより簡単にサーバーのインスタンスを作成
+// DBのインスタンスをグローバル変数に格納
+var (
+	db *gorm.DB
+)
+
+func main() {
+	// DB接続処理
+	var err error
+	// 第一引数はドライバー、第二引数は使用するテーブル
+	db, err = gorm.Open("sqlite3", "gorm.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	// サーバーが終了したらDB接続も終了する
+	defer db.Close()
+
+	// echoパッケージによりサーバーのインスタンスを作成
 	e := echo.New()
 
 	// ルーティングの設定
-	e.GET("/", helloworld)
+	e.GET("/contents", getALlContent)
+	e.POST("/create", createContent)
 
-	//サーバー起動
+	// サーバー起動
 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
-
 }
 
-//"Hello, World"と吐き出すサーバーの実装
-func helloworld(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World")
+//echoパッケージ版のハンドラ関数の実装
+func createContent(c echo.Context) error {
+	// レコード登録
+	db.Create(&Content{Text: "newText"})
+	return c.String(http.StatusOK, "record created")
+}
+
+//echoパッケージ版のハンドラ関数の実装
+func getALlContent(c echo.Context) error {
+	var content Content
+	// contentテーブルのレコードを全件取得
+	db.Find(&content)
+	// 取得したデータをJSONにして返却
+	return c.JSON(http.StatusOK, content)
 }
